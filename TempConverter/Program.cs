758462eb2910b217913aa3372b3bf3a4eec82b5d
Rick Love <ricklove@outlook.com>
2018-04-27 16:31:48 +0200

    created a simple desktop application temp converter in c#

---
+++ TempConverter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TempConverter
+{
+    class Program
+    {
+        private const string V = " is the Celcius equivalent";
+
+        static void Main()
+        {
+
+            while (true)
+            {
+                Console.Write("Enter the temp in Fahrenheit: ");
+                
+                var entry = Console.ReadLine();
+
+                if(entry.ToLower() == "quit")
+                {
+                    break;
+                }
+                try
+                {
+                    var tempToConvert = double.Parse(entry);
+                    var convertedTemp = (tempToConvert - 32) * 5 / 9;
+
+                    if(tempToConvert <= 0)
+                    {
+                        Console.WriteLine(tempToConvert + " is not a valid temperature!");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine(Math.Round(convertedTemp) + V);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not a valid entry");
+                    continue;
+                }
+            }
+        }
+    }
+}
